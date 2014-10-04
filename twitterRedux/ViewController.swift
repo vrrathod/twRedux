6d7230ce72ef22e590bf4daf730dd4bf4a12d391
vr <vr@kopi-luwak-5.local>
2014-10-04 15:49:57 -0700

    Basic hamburger menu set

--- twitterRedux/ViewController.swift
+++ twitterRedux/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - UI Outlets
+    @IBOutlet weak var contentViewXalignment: NSLayoutConstraint!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        hideHamburgerMenu()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    // MARK: - Sliding adjustment for Content View
+    func showHamburgerMenu() {
+        // TODO: move 300 to constants
+        self.contentViewXalignment.constant = -180;
+    }
+    
+    func hideHamburgerMenu() {
+        contentViewXalignment.constant = 0
+    }
+    
+    func isHamburgerMenuVisible() -> Bool {
+        return contentViewXalignment.constant != 0
+    }
+    
+    @IBAction func showHideMenu(sender: UIButton) {
+        if isHamburgerMenuVisible() {
+            hideHamburgerMenu()
+        } else {
+            showHamburgerMenu()
+        }
+    }
+    @IBAction func didSwipe(sender: UISwipeGestureRecognizer) {
+        if sender.direction == UISwipeGestureRecognizerDirection.Right {
+            if !isHamburgerMenuVisible() {
+                showHamburgerMenu()
+            }
+        } else if sender.direction == UISwipeGestureRecognizerDirection.Left {
+            if isHamburgerMenuVisible() {
+                hideHamburgerMenu()
+            }
+        }
+    }
 }
 
